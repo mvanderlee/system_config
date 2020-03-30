e88f358f3e802d6d43ca84343c0177dfe1599309
MichielVanderlee <mastermikeyboy@gmail.com>
2020-03-30 01:41:17 -0300

    fix: revert back to old docker install

--- install.sh
+++ install.sh
@@ -345,8 +345,22 @@ install_docker() {
   
   install_homebrew
 
-  brew install docker
-  brew install docker-compose
+  sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg2 \
+    software-properties-common
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
+  sudo apt-get update
+  sudo apt-get install -y docker-ce
+
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
 
   info "Installed Docker"
 }
